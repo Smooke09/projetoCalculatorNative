8d9046ccb0e16f3ef2f96ddf86453da1f0a4a1e5
Smooke09 <pedrolucas1014zz@gmail.com>
2022-11-24 10:53:10 -0300

    chore: initial settings

---
+++ App.js
@@ -0,0 +1,50 @@
+import React from "react";
+import {
+  StyleSheet,
+  Text,
+  Dimensions,
+  TouchableHighlight,
+  View,
+} from "react-native";
+import Button from "./src/Components/Button";
+import Display from "./src/Components/Display";
+
+export default App = () => {
+  state = {
+    displayValue: "0",
+  };
+  return (
+    <View style={styles.container}>
+      <Display value={this.state.displayValue} />
+      <View style={styles.buttons}>
+        <Button label="AC" />
+        <Button label="/" />
+        <Button label="7" />
+        <Button label="8" />
+        <Button label="9" />
+        <Button label="*" />
+        <Button label="4" />
+        <Button label="5" />
+        <Button label="6" />
+        <Button label="-" />
+        <Button label="1" />
+        <Button label="2" />
+        <Button label="3" />
+        <Button label="+" />
+        <Button label="0" />
+        <Button label="." />
+        <Button label="=" />
+      </View>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  buttons: {
+    flexDirection: "row",
+    flexWrap: "wrap",
+  },
+});
